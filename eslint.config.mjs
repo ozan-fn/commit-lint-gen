493ff5edbbe42a014019c8410b0773c04e46074e
dhodo999 <dashdho2@gmail.com>
2026-07-14 13:34:50 +0700

    chore(deps): update dependencies and migrate eslint config to mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+    tseslint.configs.recommended,
 ]);
